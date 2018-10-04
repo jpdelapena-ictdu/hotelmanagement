d68d3c7dcbea86e053b72aff1baf8c7c95b7a49e
Allenlenon <allenlenon5@gmail.com>
2018-10-04 19:32:31 +0800

    admin seeder added

---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'email' => 'admin@spcf-partners.com',
+            'password' => bcrypt('admin1234'),
+        ]);
+    }
+}
